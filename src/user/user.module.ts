079afffd33bd0610b23ac7b3674ffee52b286d85
avh <andreas.vanhelden@conceto.de>
2025-05-24 10:34:14 +0200

    main: reproduce the multiple instance creation

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
+import { WebModule } from 'src/web/web.module';
 
 @Module({
   providers: [UserService],
+  imports: [WebModule],
 })
 export class UserModule {}
